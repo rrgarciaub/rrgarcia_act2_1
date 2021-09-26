173c509587d4f2d043cbba67cc2464389c1d8ed6
NetAcad DEVASC <changeme@example.com>
2021-09-26 06:26:54 +0000

    2ndbranch

--- Garcia_Act2_1.py
+++ Garcia_Act2_1.py
@@ -8,3 +8,25 @@ Finals=float(input("Enter your finals score here: "))
 
 #average
 avg = (Prelims+Midterms+Semis+Finals)/4
+
+avg=("Your average is {}!" .format(avg))
+if avg>=75:
+    print("You passed!")
+    if avg>=99 and avg<=100:
+        print("Your grade is A!")
+    elif avg>=90 and avg<=98:
+        print("Your grade is B!")
+    elif avg>=80 and avg<=89:
+        print("Your grade is C!")
+    elif avg>=75 and avg<=79:
+        print("Your grade is D!")
+elif avg<75:
+    print("You have failed, remember that Failure is not your final destination.")   
+    if avg>=71 and avg<=74:
+        print("Your grade is D.")
+    elif avg>=61 and avg<=70:
+        print("Your grade is E.")
+        
+
+
+    
