539e3bedaf6956d07616eab02392efab989b947b
NetAcad DEVASC <changeme@example.com>
2021-09-26 04:40:36 +0000

    first commit

---
+++ Garcia_Act2_1.py
@@ -0,0 +1,10 @@
+print("This Program will compute student average")
+
+#Variables
+Prelims=float(input("Enter your prelims score here: "))
+Midterms=float(input("Enter your midterms score here: "))
+Semis=float(input("Enter your semis score here: "))
+Finals=float(input("Enter your finals score here: "))
+
+#average
+avg = (Prelims+Midterms+Semis+Finals)/4
